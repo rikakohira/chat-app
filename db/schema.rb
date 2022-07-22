079befd4347980b3733db5e7858a5b04f0aab761
HIRATA RIKAKO <hiratarikako@HIRATAnoMacBook-Air.local>
2022-07-22 15:52:26 +0900

    roomsテーブル修正

--- db/schema.rb
+++ db/schema.rb
@@ -22,10 +22,9 @@ ActiveRecord::Schema.define(version: 2022_07_22_033023) do
   end
 
   create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "user_id", null: false
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_rooms_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
